68b456ef1576f0ecc47d4c87eee8f5bb4a890f32
Red8Mark <work8red@gmail.com>
2023-01-18 13:32:08 +0900

    refresh

--- src/index.js
+++ src/index.js
@@ -10,31 +10,51 @@ import { menMainPage } from "./modules/mainPage/menMainPage";
 import { getData } from "./modules/getData";
 import { API_URL, DATA } from "./modules/const";
 import { createCssColors } from "./modules/createCssColors";
+import { createElement } from "./modules/createElement";
 
 const init = async () => {
-  DATA.navigation = await getData(`${API_URL}/api/categories`);
-  DATA.colors = await getData(`${API_URL}/api/colors`);
+  try {
+    router.on("*", () => {
+      renderHeader();
+      renderFooter();
+    });
+    DATA.navigation = await getData(`${API_URL}/api/categories`);
+    DATA.colors = await getData(`${API_URL}/api/colors`);
 
-  createCssColors(DATA.colors);
+    createCssColors(DATA.colors);
 
-  router.on("*", () => {
-    renderHeader();
-    renderFooter();
-  });
+    router.on("/", () => {
+      mainPage();
+    });
 
-  router.on("/", () => {
-    mainPage();
-  });
+    router.on("women", () => {
+      womenMainPage();
+    });
 
-  router.on("women", () => {
-    womenMainPage();
-  });
+    router.on("men", () => {
+      menMainPage();
+    });
 
-  router.on("men", () => {
-    menMainPage();
-  });
+    router.on("search", (data) => {
+      menMainPage();
+    });
 
-  router.resolve();
+  } catch (e) {
+    createElement(
+      "h2",
+      {
+        textContent: "Что-то пошло не так, попробуйте позже...",
+      },
+      {
+        parent: document.querySelector("main"),
+        cb(h2) {
+          h2.style.textAlign = "center";
+        },
+      }
+    );
+  } finally {
+    router.resolve();
+  }
 };
 
 init();
